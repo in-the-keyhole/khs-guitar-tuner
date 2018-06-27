98ffcf80b799a6d9d003a2a2c4dd87b86535e0ef
Anna <alscarbo@ksu.edu>
2018-06-27 16:38:56 -0500

    features added to add and edit

--- src/main/webapp/react/components/add-view.jsx
+++ src/main/webapp/react/components/add-view.jsx
@@ -1,45 +1,93 @@
 import React from 'react';
-import {Modal} from 'react-materialize';
+import { Modal } from 'react-materialize';
+import '../css/index.css';
+import '../css/materialize.min.css';
+import Icon from 'react-materialize/lib/Icon';
 
-
-class AddView extends React.Component{
-    constructor(props){
+const NOTES = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"];
+class AddView extends React.Component {
+    constructor(props) {
         super(props);
-        this.state ={
-            tuningName: '',
-            tuningNotes: ''
+        this.state = {
+            description: '',
+            notes: '',
+            errorMessageN: '',
+            saveDismiss: true,
+            errorMessageD: '',
+            list: []
         }
-        this.onChange1 = this.onChange1.bind(this);
-        this.onChange2 = this.onChange2.bind(this);
-        this.clear = this.clear.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
-    
-    onChange1(evt){
-        this.setState({tuningName:evt.target.value});
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value }, function () {
+            this.handleChangeNotes();
+            this.handleChangeDescription();
+        });
     }
-    onChange2(evt){
-        this.setState({tuningNotes:evt.target.value});
+    handleChangeDescription() {
+        if (this.state.notes.length > 3 && this.state.description.length < 1) {
+            this.setState({ errorMessageD: "Please name your tuning", saveDismiss: true });
+        }
+        else if (this.state.description.length >= 1) {
+            this.setState({ errorMessageD: '' });
+        }
     }
-    clear(){
-        this.setState({tuningName:'',tuningNotes:''});
+    handleChangeNotes() {
+        if (this.state.notes.length > 3) {
+            let noteCount = 0;
+            let list = this.state.notes.split(',');
+            for (let i = 0; i < list.length; i++) {
+                list[i] = list[i].replace(" ", "");
+                for (let k = 0; k < NOTES.length; k++) {
+                    if (list[i] === NOTES[k]) {
+                        noteCount++;
+                    }
+                }
+            }
+            if (noteCount === 6 && list.length === 6) {
+                this.setState({ errorMessageN: '', saveDismiss: false, list: list });
+            }
+            else if (noteCount < list.length - 1 || list.length > 7) {
+                this.setState({ errorMessageN: "Please enter 6 valid notes", saveDismiss: true })
+            }
+            else {
+                this.setState({ errorMessageN: '', saveDismiss: true })
+            }
+        }
     }
-    render(){
-        return(
-           <Modal 
-                header = 'Add Tuning' trigger= {<a class='btn-floating btn-large teal accent-4 tuning-add'><i class="material-icons right">add</i></a>}
+    cancel() {
+        this.setState({ description: '', notes: '', saveDismiss: true, errorMessageD: '', errorMessageN: '' });
+    }
+    handleClick(event) {
+        //send tuning name and notes to somewhere to create a new tuning  
+        alert("You submitted a tuning of " + this.state.list + " named " + this.state.description);
+        this.setState({ description: '', notes: '', saveDismiss: true, errorMessageD: '', errorMessageN: '' });
+    }
+    render() {
+        return (
+            <Modal
+                header='Add Tuning' trigger={<a class='btn-floating btn-large teal accent-4 tuning-add'><i class="material-icons right">add</i></a>}
                 actions={
-                        <div class="modal-footer">
-                            <a class="btn-small teal accent-4 modal-close"><i class="material-icons right" onClick={this.clear}>cancel</i>cancel</a>
-                            {' '}
-                            <a class="btn-small teal accent-4 modal-close"><i class="material-icons right">save</i>save</a>
-                        </div>
+                    <div class="modal-footer">
+                        <a class="btn-small teal accent-4 modal-action modal-close" onClick={() => this.cancel()}><i class="material-icons right">cancel</i>cancel</a>
+                        {' '}
+                        <a class="btn-small teal accent-4 modal-close" onClick={() => this.handleClick()} disabled={this.state.saveDismiss}><i class="material-icons right">save</i>save</a>
+                    </div>
                 }>
-                    <h5>Description</h5>
-                    <input placeholder="Tuning Name" value={this.state.tuningName} label="Tuning Name" onChange ={evt => this.onChange1(evt)} />
-                    <h5>Notes</h5>
-                    <input placeholder="e, a, d, g, b, e" value={this.state.tuningNotes}label="Notes" onChange ={evt => this.onChange2(evt)}/>
-          </Modal>
+                <h5>Description</h5>
+                <div>
+                    <input type="text" placeholder="Tuning Name" name="description" value={this.state.description} onChange={(value) => this.onChange(value)} />
+                    <label>{this.state.errorMessageD}</label>
+                </div>
+                <h5>Notes</h5>
+                <div>
+                    <input type="text" placeholder="e, a, d, g, b, e" name="notes" value={this.state.notes} onChange={(value) => this.onChange(value)} />
+                    <label>{this.state.errorMessageN}</label>
+                </div>
+            </Modal>
         );
     }
 }
+
+
 export default AddView;
--- src/main/webapp/react/components/edit-view.jsx
+++ src/main/webapp/react/components/edit-view.jsx
@@ -3,23 +3,70 @@ import { Modal } from 'react-materialize';
 import '../css/index.css';
 import '../css/materialize.min.css';
 
+const NOTES = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"];
 class Edit extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
             description: this.props.description,
             notes: this.props.notes,
+            errorMessN: '',
+            errorMessD: '',
+            saveDismiss: true,
+            list: []
         };
+        this.onChange - this.onChange.bind(this);
     }
     onChange(e) {
-        this.setState({[e.target.name]: e.target.value})
+        this.setState({ [e.target.name]: e.target.value }, function () {
+            this.handleChangeN();
+            this.handleChangeD();
+        });
+    }
+    handleChangeD() {
+        if (this.state.notes.length > 3 && this.state.description.length < 1) {
+            this.setState({ errorMessD: "Please name your tuning", saveDismiss: true });
+        }
+        else if (this.state.description.length >= 1) {
+            this.setState({ errorMessD: '' });
+        }
+    }
+    handleChangeN() {
+        if (this.state.notes.length > 3) {
+            let noteCount = 0;
+            let list = this.state.notes.split(',');
+            for (let i = 0; i < list.length; i++) {
+                list[i] = list[i].replace(" ", "");
+                for (let k = 0; k < NOTES.length; k++) {
+                    if (list[i] === NOTES[k]) {
+                        noteCount++;
+                    }
+                }
+            }
+            if (noteCount === 6 && list.length === 6) {
+                this.setState({ errorMessN: '', saveDismiss: false, list: list });
+            }
+            else if (noteCount < list.length - 1 || list.length > 7) {
+                this.setState({ errorMessN: "Please enter 6 valid notes", saveDismiss: true });
+            }
+            else {
+                this.setState({ errorMessN: '', saveDismiss: true });
+            }
+        }
     }
     cancel() {
-        this.setState({description: this.props.description, notes: this.props.notes});
+        this.setState({
+            description: this.props.description, notes: this.props.notes, saveDismiss: true,
+            errorMessD: '', errorMessN: ''
+        });
     }
-    
-    render(){
-        return(
+    handleClick(event) {
+        //send tuning name and notes to somewhere to create a new tuning  
+        alert("You submitted a tuning of " + this.state.list + " named " + this.state.description);
+        this.setState({ saveDismiss: true, errorMessageD: '', errorMessageN: '' });
+    }
+    render() {
+        return (
             <Modal
                 header='Edit Tuning'
                 trigger={<a class="btn-small teal accent-4"><i class="material-icons right">edit</i>edit</a>}
@@ -27,13 +74,21 @@ class Edit extends React.Component {
                     <div class="modal-footer">
                         <a class="btn-small teal accent-4 modal-close" onClick={() => this.cancel()}><i class="material-icons right">cancel</i>cancel</a>
                         {' '}
-                        <a class="btn-small teal accent-4 modal-close"><i class="material-icons right">save</i>save</a>
+                        <a class="btn-small teal accent-4 modal-close" disabled={this.state.saveDismiss} onClick={() => this.handleClick()}><i class="material-icons right">save</i>save</a>
                     </div>
                 }>
                 <h5>Description</h5>
-                <input type="text" name="description" value={this.state.description} onChange={(value) => this.onChange(value)}/>
-                <h5>Notes</h5>
-                <input type="text" name="notes" value={this.state.notes} onChange={(value) => this.onChange(value)}/>
+                <div>
+                    <input type="text" name="description" value={this.state.description} onChange={(value) => this.onChange(value)}
+                        placeholder="Tuning Name" />
+                    <label>{this.state.errorMessD}</label>
+                </div>
+                <div>
+                    <h5>Notes</h5>
+                    <input type="text" name="notes" value={this.state.notes} onChange={(value) => this.onChange(value)}
+                        placeholder="e, a, d, g, b, e" />
+                    <label>{this.state.errorMessN}</label>
+                </div>
             </Modal>
         );
     }
