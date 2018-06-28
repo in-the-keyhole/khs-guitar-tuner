f83cf8098f662a348e6e53555917d4f5c1721a05
jdehan2 <39960045+jdehan2@users.noreply.github.com>
2018-06-28 13:32:32 -0500

    tuning-view update

--- src/main/webapp/react/components/tunings-view.jsx
+++ src/main/webapp/react/components/tunings-view.jsx
@@ -5,23 +5,31 @@ import AddView from './add-view';
 import PageFooter from './footer.jsx';
 import '../css/index.css';
 
-const TUNINGS = [
-    {id: 1001, description: 'Standard', notes: 'e, a, d, g, b, e'},
-    {id: 1002, description: 'DROP D', notes: 'd, a, d, g, b, e'},
-    {id: 1003, description: 'DADGAD', notes: 'd, a, d, g, a, d'},
-    {id: 1004, description: 'Half Step Down', notes: 'd#, g#, c#, f#, a#, d#'},
-    {id: 1005, description: 'Whole Step Down', notes: 'd, g, c, f, a, d'},
-    {id: 1006, description: 'OPEN C', notes: 'c, g, c, g, c, e'},
-    {id: 1007, description: 'OPEN D', notes: 'd, a, d, f#, a, d'},
-    {id: 1008, description: 'OPEN G', notes: 'd, g, d, g, b, d'}
-];
 
 class TuningsView extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			tuningsArray: [],
+		};
+	}
+	
+	componentWillMount() {
+		fetch('http://localhost:8765/tunings')
+		.then(results => { return results.json(); })
+		.then(data => {
+			this.setState({tuningsArray: data._embedded.tunings});
+		})
+		.catch((error) => {console.log(error)});
+		
+	}
     render(){
         return(
             <div className='page-view'>
                 <PageHeader title='Keyhole Guitar Tuner'/>
-                <TuningList tunings={TUNINGS} />
+                {//<TuningList tunings={this.props.tunings} />
+                }
+                <TuningList tunings={this.state.tuningsArray} />
                 <AddView/>
                 <PageFooter />
             </div>
