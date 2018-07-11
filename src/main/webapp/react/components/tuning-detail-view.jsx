a578df5d8ee93791645095a99612763428ecd3c5
jdehan2 <39960045+jdehan2@users.noreply.github.com>
2018-07-11 12:02:24 -0500

    Database testing 1.3

--- src/main/webapp/react/components/tuning-detail-view.jsx
+++ src/main/webapp/react/components/tuning-detail-view.jsx
@@ -32,7 +32,7 @@ class PageDetailView extends React.Component {
     }
     componentWillMount() {
         let currentComponent = this;
-        fetch( 'postgresql://ec2-50-19-105-188.compute-1.amazonaws.com:5432/dc7snietmf0at2?sslmode=require' )
+        fetch( 'jdbc:postgresql://ec2-50-19-105-188.compute-1.amazonaws.com:5432/dc7snietmf0at2?user=peougkqatnnvjb&password=9036dc1d392c9afe5c94b0ceb66480ce0cc5a20767ba3b97bad777a7872ea2df&sslmode=require' )
             .then( results => { return results.json(); } )
             .then( data => {
                 let tunings = data._embedded.tunings;
