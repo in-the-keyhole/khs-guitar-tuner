2d8245d2e5b4fb932a6a699302d88589154fde6e
jdehan2 <39960045+jdehan2@users.noreply.github.com>
2018-07-24 10:30:53 -0500

    Mobile Login hide

--- src/main/webapp/react/components/header.jsx
+++ src/main/webapp/react/components/header.jsx
@@ -31,7 +31,7 @@ class PageHeader extends React.Component {
                     <a className="header-title">{this.props.title}</a>
                     <ul class="right">
                         <li className='header-btn'>{this.props.isMainPage && this.props.isAdmin && <LogoutView Logout = {() => this.props.Logout()}/>}</li>
-                        <li className='header-btn'>{this.props.isMainPage && !this.props.isAdmin && <LoginView Login = {() => this.props.Login()}/>}</li>
+                        <li className='header-btn'>{this.props.isMainPage && !this.props.isAdmin && <LoginView Login = {() => this.props.Login()}/>} class="hide-on-small-only"></li>
                         <li className='header-btn header-about'><NavLink to="/about"><i class="small material-icons">help_outline</i></NavLink></li>
                     </ul>
                 </div>
