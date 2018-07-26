e241051bfadba889cb71c599e904bce1b6ae05d7
Anna <alscarbo@ksu.edu>
2018-07-26 16:03:32 -0500

    revert express test

--- src/index.js
+++ src/index.js
@@ -20,19 +20,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from './main/webapp/react/components/nav.jsx';
 
-const express = require('express')
-const path = require('path')
-const app = express()
 
-// serve static assets normally
-app.use(express.static(__dirname + '/public'))
-
-// Handles all routes so you do not get a not found error
-app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
-
-app.listen(port)
-console.log("server started on port " + port)
 
 ReactDOM.render(<Navigation />, document.getElementById('root'));
